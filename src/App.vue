54b727318c9e9708e0f80b289836df106a050f49
Andrey <antonov.andrey.10101@gmail.com>
2023-09-12 10:14:36 +0300

    feat: add Result

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,8 @@
-<template><Form /></template>
+<template>
+  <div class="container">
+    <Form />
+  </div>
+</template>
 
 <script lang="ts">
 import Form from "./components/Form.vue";
@@ -11,5 +15,16 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="scss" sroped>
+.container {
+  background-color: #fdfdfd;
+  display: flex;
+  flex-direction: column;
+  width: 600px;
+  margin: 1.25rem;
+  height: auto;
+  padding: 2.5rem;
+  border-radius: 1.875rem;
+  border-bottom-right-radius: 8.125rem;
+}
 </style>
---
+++ src/components/Result.vue
@@ -0,0 +1,43 @@
+<template>
+     <div>
+        <ul class="result__list">
+          <li class="result__item">
+            <!-- <span :class="calculate[item] ? '' : 'tracking-[15px]'">{{ calculate[item] ?? '--'}}</span> -->
+            <!-- <span class="result__text">{{item}}</span> -->
+          </li>
+        </ul>
+      </div>
+    </div>
+  </template>
+    
+  <script lang="ts">
+export default {
+  name: "Result",
+  setup() {
+    return {};
+  },
+};
+</script>
+    
+<style lang="scss" scoped>
+.result__list {
+  display: flex;
+  flex-direction: column;
+}
+
+.result__item {
+  font-weight: bold;
+  font-size: 6rem;
+  font-style: italic;
+  color: #6b46c1;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+}
+.result__text {
+  color: #333333;
+  font-weight: 900;
+  padding-left: 0.25rem;
+}
+</style>
+    
